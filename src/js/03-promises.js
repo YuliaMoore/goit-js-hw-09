8f9b6d534debfe33c85d1586a8aac7036ed72eb8
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-04-25 09:57:21 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,32 @@
 const form = document.querySelector('.form');
 form.addEventListener('submit', onFormSubmit);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  let { delay, step, amount } = form.elements;
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+}
