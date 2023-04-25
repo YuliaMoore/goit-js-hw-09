87ea6038082a5dc99c4a6cd7c7247859218a7258
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-04-25 06:13:24 +0000

    Deploying to gh-pages from @ YuliaMoore/goit-js-hw-09@3bf7c2bb29e9528ee393fb0d6c43009dc8467d42 🚀

---
+++ 03-promises.f6ca6fcb.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",onFormSubmit),(void Math.random()).then((function(o){var n=o.position,e=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(e,"ms"))})).catch((function(o){var n=o.position,e=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(e,"ms"))}));
+//# sourceMappingURL=03-promises.f6ca6fcb.js.map
